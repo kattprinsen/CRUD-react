0a5a3636655e1b3a3d320030c2f2bf73f83232db
simonbergqvist <simon.bergqvist@consid.se>
2021-10-07 09:48:17 +0200

    Added a navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+
+import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Devices } from './components/Devices';
+import { Sites } from './components/Sites';
+import { Users } from './components/Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/sites"} className="nav-link">
+              Sites
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/devices"} className="nav-link">
+              Devices
+            </Link>
+          </li>
+        </div>
+      </nav>
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/sites"]} component={Sites} />
+            <Route exact path="/devices" component={Devices} />
+            <Route path="/devices/:id" component={Devices} />
+          </Switch>
+        </div>
     </div>
   );
 }

0d132cfd59097e677cd6de5deb019f60b23f7683
simonbergqvist <simon.bergqvist@consid.se>
2021-10-07 12:45:55 +0200

    Clicking on a device now shows more information

--- src/components/Devices.js
+++ src/components/Devices.js
@@ -4,6 +4,7 @@ import ApiService from '../services/ApiService';
 const Devices = () => {
 
   const [devices, setDevices] = useState([]);
+  const [currentDevice, setCurrentDevice] = useState(null);
 
   useEffect(() => {
     showDevices();
@@ -16,16 +17,51 @@ const Devices = () => {
       });
   };
 
+  const setActiveDevice = (device) => {
+    console.log(device);
+    setCurrentDevice(device);
+  }
+
   return(
     <div className="devices">
       <ul className="list-group">
         {devices.map((device, index) => (
-          <li className="list-group-item" /*onClick={() => setDevice()}*/ key={index}>
+          <li className="list-group-item" onClick={() => setActiveDevice(device)} key={index}>
             {device.title}
           </li>
         ))}
       </ul>
-    </div>
+        <div className="col-md-6">
+        {currentDevice ? (
+          <div>
+            <h4> Device information </h4>
+            <div>
+              <label>
+                <strong> Title: </strong>
+              </label>{" "}
+              {currentDevice.title}
+            </div>
+            <div>
+              <label>
+                <strong> Description: </strong>
+              </label>{" "}
+              {currentDevice.description}
+            </div>
+            <div>
+              <label>
+                <strong> Model: </strong>
+              </label>{" "}
+              {currentDevice.model}
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p> Please click on something </p>
+          </div>
+        )}
+        </div>
+      </div>
+
   );
 
 };
